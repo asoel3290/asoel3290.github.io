9a9f337d54fa3e7ea5ea1436f36101d252531e03
Lintang-Andika <lintang.andika@colorado.edu>
2022-11-15 17:42:12 -0700

    Map Works

--- js/main.js
+++ js/main.js
@@ -1,1 +1,193 @@
 //insert code here!
+//declare map variable globally so all functions have access
+var map;
+var attrArray = ["1", "2", "3", "4", "7"];
+var expressed = attrArray[0];
+
+var counties = document.getElementById("counties");
+$.getJSON("data/FloridaCounties.geojson", function (response) {
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(response, {
+        onEachFeature: onEachFeature
+        /*style: function (feature) {
+            return {
+                color: "black",
+                fillColor: "black",
+                fillOpacity: "80%"
+            }
+        }*/
+    }).addTo(map);
+
+});
+var one_ft = document.getElementById("one_ft");
+$.getJSON("data/FloridaCounties.geojson", function (response) {
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(response, {
+        onEachFeature: onEachFeature
+        /*style: function (feature) {
+            return {
+                color: "blue",
+                fillColor: "blue",
+                fillOpacity: "0%"
+            }
+        }*/
+    }).addTo(map);
+
+});
+
+var two_ft = document.getElementById("two_ft");
+$.getJSON("data/FloridaCounties.geojson", function (response) {
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(response, {
+        onEachFeature: onEachFeature
+        /*style: function (feature) {
+            return {
+                color: "pink",
+                fillColor: "pink",
+                fillOpacity: "50%"
+            }
+        }*/
+    }).addTo(map);
+    createSequenceControls();
+});
+
+//create the map
+var map = L.map('map', {
+    center: [26.3014, -80.6321],
+    zoom: 7
+});
+
+//add OSM base tilelayer
+var usmap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+}).addTo(map);
+
+
+// specify the basemap and overlays to put in the layers control
+var baseMaps = {
+    "US": usmap,
+};
+
+var overlayMaps = {
+    "Counties": counties,
+    "one_ft": one_ft,
+    "two_ft": two_ft
+};
+
+//L.control.layers(overlayMaps).addTo(map);
+
+//function to instantiate the Leaflet map
+function createMap() {
+
+    getData(map);
+
+};
+
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties) {
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
+};
+
+function createSequenceControls() {
+    //create range input element (slider)
+    var SequenceControl = L.Control.extend({
+        options: {
+            position: 'bottomleft'
+        },
+
+        onAdd: function () {
+            // create the control container div with a particular class name
+            var container = L.DomUtil.create('div', 'sequence-control-container');
+
+            // ... initialize other DOM elements
+            $(container).append('<input class="range-slider" type="range">');
+            $(container).append('<button class="step" id="reverse" title="Reverse">Reverse</button>');
+            $(container).append('<button class="step" id="forward" title="Forward">Forward</button>');
+
+
+            L.DomEvent.disableClickPropagation(container);
+
+            return container;
+        }
+    });
+    map.addControl(new SequenceControl());
+    $('.range-slider').attr({
+        max: 2,
+        min: 0,
+        value: 0,
+        step: 1
+    });
+    $('.step').click(function () {
+        //sequence
+        //get the old index value
+        var index = $('.range-slider').val();
+
+        //Step 6: increment or decrement depending on button clicked
+        if ($(this).attr('id') == 'forward') {
+            index++;
+            //Step 7: if past the last attribute, wrap around to first attribute
+            index = index > 2 ? 0 : index;
+        } else if ($(this).attr('id') == 'reverse') {
+            index--;
+            //Step 7: if past the first attribute, wrap around to last attribute
+            index = index < 0 ? 2 : index;
+        };
+
+        //Step 8: update slider
+        $('.range-slider').val(index);
+
+        //Step 9: pass new attribute to update symbols
+        updatePropSymbols(index);        
+    });
+    $('.range-slider').on('input', function(){
+        //sequence
+        var index = $(this).val();        
+        updatePropSymbols(index); 
+    });
+
+    //replace button content with images
+    $('#reverse').html('<img src="img/reverse.png">');
+    $('#forward').html('<img src="img/forward.png">');
+};
+
+function updatePropSymbols(index) {
+
+    if (index == 0) {
+
+        counties.opacity = "100%";
+        one_ft.opacity = "0%";
+        two_ft.opacity = "0%";
+
+    } else if (index == 1) {
+
+        counties.opacity = "0%";
+        one_ft.opacity = "100%";
+        two_ft.opacity = "0%";
+
+    } else if (index == 2) {
+
+        counties.opacity = "0%";
+        one_ft.opacity = "0%";
+        two_ft.opacity = "100%";
+
+    };
+
+};
+
+//function to retrieve the data and place it on the map
+function getData(map) {
+    // parse the geojson (very simplified example here)
+    counties;
+    one_ft;
+    two_ft;
+}
+
+$(document).ready(createMap);
+//$(document).ready(createSequenceControls(map));
